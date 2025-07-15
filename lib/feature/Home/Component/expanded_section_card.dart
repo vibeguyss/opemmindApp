e4ef491362e884e34d80af467e2f6f487e8007c0
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-07-15 17:17:16 +0900

    homeView 끝

---
+++ lib/feature/Home/Component/expanded_section_card.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:openmind_app/shared/FontExt.dart'; // FontExt 경로에 따라 수정
+
+class ExpandedSectionCard extends StatelessWidget {
+  final String headerText;
+  final String title;
+  final IconData headerIcon;
+  final Color headerIconColor;
+  final Color cardColor;
+  final Color titleTextColor;
+  final Color headerTextColor;
+  final String buttonText;
+  final Color buttonColor;
+  final Color buttonTextColor;
+  final VoidCallback onButtonPressed;
+  final List<Widget>? additionalActions;
+
+  const ExpandedSectionCard({
+    Key? key,
+    required this.headerText,
+    required this.title,
+    required this.headerIcon,
+    this.headerIconColor = const Color(0xFF90CAF9),
+    this.cardColor = Colors.white,
+    this.titleTextColor = Colors.black,
+    this.headerTextColor = Colors.black54,
+    required this.buttonText,
+    this.buttonColor = Colors.black,
+    this.buttonTextColor = Colors.white,
+    required this.onButtonPressed,
+    this.additionalActions,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: cardColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      child: Padding(
+        padding:  EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  headerText,
+                  style: AppFont.regular(14).copyWith(color: headerTextColor),
+                ),
+                Icon(headerIcon, color: headerIconColor, size: 30),
+              ],
+            ),
+             SizedBox(height: 5),
+            Text(
+              title,
+              style: AppFont.bold(22).copyWith(color: titleTextColor),
+            ),
+             SizedBox(height: 20),
+            Center(
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onButtonPressed,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: buttonColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                  ),
+                  child: Text(
+                    buttonText,
+                    style: AppFont.bold(18).copyWith(color: buttonTextColor),
+                  ),
+                ),
+              ),
+            ),
+            // THIS PART IS MISSING!
+            if (additionalActions != null && additionalActions!.isNotEmpty) ...[
+              const SizedBox(height: 20),
+              ...additionalActions!,
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ActionRow extends StatelessWidget {
+  final String mainText;
+  final String actionText;
+  final VoidCallback onTap;
+  final Color textColor;
+
+  const ActionRow({
+    Key? key,
+    required this.mainText,
+    required this.actionText,
+    required this.onTap,
+    this.textColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              mainText,
+              style: AppFont.medium(16).copyWith(color: textColor),
+            ),
+            Row(
+              children: [
+                Text(
+                  actionText,
+                  style: AppFont.medium(
+                    16,
+                  ).copyWith(color: textColor.withOpacity(0.54)),
+                ),
+                SizedBox(width: 5),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: textColor.withOpacity(0.54),
+                  size: 16,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/Home/HomeScreen.dart
+++ lib/feature/Home/HomeScreen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:openmind_app/feature/AI/AIScreen.dart';
 import 'package:openmind_app/feature/Component/SearchTextField.dart';
+import 'package:openmind_app/feature/Home/Component/expanded_section_card.dart';
+import 'package:openmind_app/feature/Message/MessageScreen.dart';
+import 'package:openmind_app/feature/Write/WriteScreen.dart';
 import 'package:openmind_app/shared/ColorExt.dart';
 import 'package:openmind_app/shared/FontExt.dart';
 import 'package:openmind_app/shared/IconExt.dart';
@@ -31,6 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,10 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverAppBar(
             backgroundColor: AppColor.background,
             centerTitle: false,
-            title: Text(
-              "OPENMIND",
-              style: AppFont.bold(20),
-            ),
+            title: Text("OPENMIND", style: AppFont.bold(20)),
             actions: [
               IconButton(
                 onPressed: () {
@@ -62,7 +64,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
           SliverToBoxAdapter(
             child: Padding(
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               child: SizedBox(
                 height: 40,
                 child: SearchTextField(
@@ -75,6 +77,140 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
+          ),
+
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: ExpandedSectionCard(
+                  headerText: "새로운 마음을 찾아요",
+                  title: "AI랑 상담하러 가기",
+                  headerIcon: Icons.psychology_outlined,
+                  headerIconColor: Color(0xFF81D4FA),
+                  cardColor: Colors.white,
+                  titleTextColor: Colors.black,
+                  headerTextColor: Colors.black54,
+                  buttonText: "AI 상담 시작하기",
+                  buttonColor: Colors.black,
+                  buttonTextColor: Colors.white,
+                  onButtonPressed: () {
+                    print("AI 상담 시작하기");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AIScreen()),
+                    );
+                  },
+                  additionalActions: [
+                    ActionRow(
+                      mainText: "AI와 대화 기록 보기",
+                      actionText: "기록 확인",
+                      onTap: () {
+                        print("AI와 대화 기록 보기 (AIScreen으로 이동)");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => AIScreen()),
+                        );
+                      },
+                      textColor: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: ExpandedSectionCard(
+                  headerText: "나에게 맞는 상담사를 찾아요",
+                  title: "상담사 매칭 하러 가기",
+                  headerIcon: Icons.people_alt_outlined,
+                  headerIconColor: Color(0xFFAED581),
+                  cardColor: Colors.white,
+                  titleTextColor: Colors.black,
+                  headerTextColor: Colors.black54,
+                  buttonText: "상담사 매칭 시작",
+                  buttonColor: Colors.black,
+                  buttonTextColor: Colors.white,
+                  onButtonPressed: () {
+                    print("상담사 매칭 시작");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const MessageScreen(),
+                      ),
+                    );
+                  },
+                  additionalActions: [
+                    ActionRow(
+                      mainText: "상담사 프로필 둘러보기",
+                      actionText: "프로필 확인",
+                      onTap: () {
+                        print("상담사 프로필 둘러보기 (MessageScreen으로 이동)");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const MessageScreen(),
+                          ),
+                        );
+                      },
+                      textColor: Colors.black,
+                    ),
+                    Divider(color: Colors.black12, height: 20),
+                    ActionRow(
+                      mainText: "상담사 확인",
+                      actionText: "상담사 확인 하러 가기",
+                      onTap: () {
+                        print("상담 예약 내역 확인 (MessageScreen으로 이동)");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const MessageScreen(),
+                          ),
+                        );
+                      },
+                      textColor: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: ExpandedSectionCard(
+                  headerText: "오늘 하루를 기록해요",
+                  title: "일기 쓰러 가기",
+                  headerIcon: Icons.book_outlined,
+                  headerIconColor: Color(0xFFFFCC80),
+                  cardColor: Colors.white,
+                  titleTextColor: Colors.black,
+                  headerTextColor: Colors.black54,
+                  buttonText: "새 일기 작성하기",
+                  buttonColor: Colors.black,
+                  buttonTextColor: Colors.white,
+                  onButtonPressed: () {
+                    print("새 일기 작성하기");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => WriteScreen()),
+                    );
+                  },
+                  additionalActions: [
+                    ActionRow(
+                      mainText: "나의 일기 모아보기",
+                      actionText: "전체 보기",
+                      onTap: () {
+                        print("나의 일기 보러가기 (WriteScreen으로 이동)");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => WriteScreen(),
+                          ),
+                        );
+                      },
+                      textColor: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
           ),
         ],
       ),
